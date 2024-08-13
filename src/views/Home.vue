9722917a6303dfe6e6720a1bbf05296ba391351a
VictorTimbo <manoelvictortimbo@gmail.com>
2024-08-12 22:15:15 -0300

    init branch develop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@ export default defineComponent({})
 </script>
 
 <template>
-  <main>
-    NBA
+  <main class="flex min-w-full min-h-screen items-center justify-center bg-gradient-to-tr from-amber-700 via-yellow-600 to-orange-500">
+    <h1 class="text-white text-8xl font-mono font-black">Estatísticas - NBA</h1>
   </main>
 </template>
