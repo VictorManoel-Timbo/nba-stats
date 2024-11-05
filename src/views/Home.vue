418b547dc44c531f948e01f6389cc30a55bd5235
victor timbo <manoelvictortimbo@gmail.com>
2024-11-05 13:16:31 -0300

    att

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,9 +9,9 @@ export default defineComponent({
   data() {
     return {
       nav: [
-        { name: 'Times', link: '/Times' },
-        { name: 'Jogadores', link: '/Jogadores' },
-        { name: 'Jogos', link: '/Jogos' }
+        { name: 'Times', link: '/teams' },
+        { name: 'Jogadores', link: '/players' },
+        { name: 'Jogos', link: '/games' }
       ],
       selectedContent: '',
       isContentVisible: false,
@@ -19,7 +19,7 @@ export default defineComponent({
   },
   methods: {
     showContent(item: string) {
-      this.selectedContent = item
+      this.selectedContent = item;
       this.isContentVisible = true;
     },
     hideContent(item: string) {
@@ -36,15 +36,14 @@ export default defineComponent({
       <span class="flex justify-end hover:text-blue-600 duration-300">NBA</span>
     </section>
     <div class="flex items-center justify-around font-tomorrow w-[70%]">
-      <RouterLink v-for="item in nav" :key="item.name" :to="item.link" class="btn" @mouseenter="showContent(item.name)"
+      <RouterLink v-for="item in nav" :key="item.name" :to="item.link" class="relative inline-flex items-center justify-center px-6 py-3 text-2xl font-semibold cursor-pointer hover:text-orange-500" @mouseenter="showContent(item.name)"
         @mouseleave="hideContent(item.name)">
         {{ item.name }}
-        <span class="before-content"></span>
       </RouterLink>
     </div>
     <section class="w-full h-[50rem] p-5 bg-black rounded-3xl relative">
       <transition name="fade">
-        <div v-show="isContentVisible" class="border-4 rounded-lg border-orange-500 h-[100%] w-[100%] p-[5%] overflow-hidden  opacity-100">
+        <div v-show="isContentVisible" class="border-4 rounded-lg border-orange-500 h-[100%] w-[100%] p-[5%] overflow-hidden opacity-100">
             <Resumes :resumeType="selectedContent" />
         </div>
       </transition>
@@ -53,43 +52,6 @@ export default defineComponent({
 </template>
 
 <style scoped>
-.btn {
-  position: relative;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 20px 20px 0px 0px;
-  padding: 12px 24px;
-  font-size: 1.45rem;
-  font-weight: 500;
-  color: black;
-  text-decoration: none;
-  overflow: hidden;
-  cursor: pointer;
-  z-index: 1;
-  transition: color 250ms ease-in-out;
-}
-
-.before-content {
-  content: "";
-  position: absolute;
-  inset: 0;
-  background-color: black;
-  transform: scaleY(0);
-  transform-origin: bottom;
-  transition: transform 250ms ease-in-out;
-  z-index: -1;
-}
-
-.btn:hover {
-  color: white;
-}
-
-.btn:hover .before-content {
-  transform: scaleY(1);
-  transform-origin: top;
-}
-
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s ease, transform 0.5s ease;
